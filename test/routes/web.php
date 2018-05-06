99fbdb488f79abf8a99fafdcc2192b67042e4ac0
Md. Yusuf Khan <md.yusuf.khan.2660@gmail.com>
2018-05-06 13:31:20 +0600

    eloquent test

--- test/routes/web.php
+++ test/routes/web.php
@@ -10,15 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
 
 Route::get('/tasks', function () {
-	$tasks = DB::table('tasks')->get();
+	//$tasks = DB::table('tasks')->get();
+	$tasks = Task::all();
 
     return view('tasks/index', compact('tasks'));
 });
 Route::get('/tasks/{task}', function ($id) {
-	$task = DB::table('tasks')->find($id);
-
+	//$task = DB::table('tasks')->find($id);
+	$task = Task::find($id);
     return view('tasks/show', compact('task'));
 });
 
